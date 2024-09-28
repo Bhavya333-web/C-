7ee93405427fab144b4be70209b87cd5b932d891
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-09-28 22:02:16 +0530

    vowel

---
+++ .vscode/practice3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cout<<"write an word"<<endl;
+    getline(cin,s);
+    int vowelcount = 0 ;
+    for(int i=0; i<s.length(); i++){
+    char ch =s[i];
+    if( ch=='a'||  ch=='e'||  ch=='i' || ch=='o'|| ch=='u'|| ch=='A' || ch=='E' ||  ch=='I' || ch=='O'|| ch=='U' ){
+        vowelcount ++;
+        cout<<"no. of vowels"<<vowelcount<<endl;
+
+    }
+    }
+
+
+}
