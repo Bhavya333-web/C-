859c5f9750db909141e69985b5424eb78b8c91ea
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-09-29 21:38:26 +0530

    conditional

---
+++ .vscode/practice6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if(n>=1 && n<=9){
+    string numbers[] = {"one","two","three","four","five","six","seven","eight","nine"};
+    cout << numbers[n-1] <<endl;
+    }
+    else{
+    cout<< "Greater than 9" <<endl;
+    }
+    return 0;
+}
+    
