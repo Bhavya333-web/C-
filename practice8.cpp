92c5f2d8457c4496f5683e5d95835138a2d924ba
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-09-30 23:29:10 +0530

    c++

---
+++ practice8.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int A;
+int B;
+
+
+for(int i =0;i<3;i++){
+    cin>>A>>B;
+    
+    cout<<A*A+2*A*B+B*B<<endl;
+    cout<<A+B<<endl;
+    
+}
+
+
+}
