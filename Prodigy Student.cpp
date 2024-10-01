6953367205f1e063cbe3e53b320a79268444ceb2
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-02 00:11:47 +0530

    c++

---
+++ Prodigy Student.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string student1;
+	string student2;
+	cin>>student1>>student2;
+	int x;
+	int y;
+	cin>>x>>y;
+	if(x>y){
+	    cout<<student1;
+	}
+	else if(y>x){
+	    cout<<student2;
+	}
+else if(y==x){
+    cout<<"equal";
+}
+}
