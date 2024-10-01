22dc1f43cc556a65180023349b602bb8ec9d6c7f
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-02 00:04:30 +0530

    c++

---
+++ practice11.cpp
@@ -0,0 +1,22 @@
+ #include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+int x;
+cin>>x;
+if(x>90){
+    cout<<"A";
+    
+}
+else if(x>70){
+    cout<<"B";
+    
+}
+else if (x>=40){
+    cout<<"C";
+}
+else{
+    cout<<"F";
+}
+}
