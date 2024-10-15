7ff4d770bc7e9e6a66ca28eff06800c4fba70b12
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-15 22:06:44 +0530

    c++

---
+++ Q_2ndMaxNoBySorting.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++) {
+        int nums[3];
+        cin >> nums[0] >> nums[1] >> nums[2];
+        sort(nums, nums + 3);
+        cout << nums[1] << endl; 
+    }
+    return 0;
+}
