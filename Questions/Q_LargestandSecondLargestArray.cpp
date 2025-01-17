54f19063b0fd3a324d582cf0166cd6d935cd8357
Bhavya Mittal <bhavyamittal575@gmail.com>
2025-01-17 20:02:34 +0530

    array

--- Questions/Q_LargestandSecondLargestArray.cpp
+++ Questions/Q_LargestandSecondLargestArray.cpp
@@ -6,8 +6,9 @@ int main(){
     cin>>t;
     while(t--){
         int n;
-        int a[n];
         cin>>n;
+        int a[n];
+        
         for(int i=0;i<n;i++){
             cin>>a[i];
         }
