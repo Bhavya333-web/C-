33c0a4ce7c06a212c7f5f5796de134d74573da07
Bhavya Mittal <bhavyamittal575@gmail.com>
2025-01-17 19:55:36 +0530

    array

---
+++ Questions/Q_Largest&SecondLargestArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        int a[n];
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int maxno = INT_MIN;
+        int secno = INT_MIN;
+        for(int i=0;i<n;i++){
+            if(a[i]>maxno){
+                secno = maxno;
+                maxno = a[i];
+            }
+            else if(a[i]>secno && a[i]!=maxno){
+                secno = a[i];
+            }
+        }
+        cout<<maxno+secno<<endl;
+
+            }
+}
