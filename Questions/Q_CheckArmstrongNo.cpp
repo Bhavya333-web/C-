a36780f48d68bc520bea0602bc83b180cf56b5a9
Bhavya Mittal <bhavyamittal575@gmail.com>
2025-01-18 20:48:17 +0530

    cpp

--- Questions/Q_CheckArmstrongNo.cpp
+++ Questions/Q_CheckArmstrongNo.cpp
@@ -9,7 +9,7 @@ int main(){
     int originaln = n;
     while(n>0){
         lastdigit = n%10;
-        sum += pow(lastdigit,3);
+        sum += lastdigit*lastdigit*lastdigit;
             n = n/10;
     }
     if(sum ==originaln){
